055e95ed99d19e9f8c87da6b26198342f1ccdf80
Nathan Ross <nross.se@gmail.com>
2014-06-27 16:49:43 -0400

    perf compile, + more DRY

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,36 @@
 var gulp = require('gulp');
-
+//var argv = require('yargs').argv;
 var uglify = require('gulp-uglify');
 var sweetjs = require('gulp-sweetjs');
 var mixotroph = require('gulp-mixotroph');
 
-gulp.task('compileSpecs', function() {
+function compileCore(mode) {
   return gulp.src("src/benches/*.js")
-    .pipe(mixotroph({mode:"spec",snippetdir:"src/macros/snippets"}))
-    .pipe(sweetjs({modules: ['./node_modules/minimalasm/mnlasm.sjs'], readableNames: true}))
-    .pipe(uglify())
-    .pipe(gulp.dest("gen/specs"));
+      .pipe(mixotroph({mode:"SPEC",snippetdir:"src/macros/snippets"}))
+      .pipe(sweetjs({modules: ['./node_modules/minimalasm/mnlasm.sjs'], readableNames: true}));
+}
+
+gulp.task('compileSpecs', function() {
+  return compileCore("SPEC").pipe(gulp.dest("gen/spec"));
 });
+gulp.task("compileSpecsMin", function() {
+    return compileCore("SPEC").pipe(uglify())
+    .pipe(gulp.dest("gen/spec"));
+});
+
+gulp.task('compilePerf', function() {
+  return compileCore("PERF").pipe(uglify())
+    .pipe(gulp.dest("gen/spec"));
+});
+
+gulp.task('cleanSpecs', function() {
+  return gulp.src("gen/spec/*.js", {read: false})
+    .pipe(clean());
+});
+
+gulp.task('cleanPerf', function() {
+  return gulp.src("gen/perf/*.js", {read: false})
+    .pipe(clean());
+});
+
+gulp.task('cleanAll', ["cleanSpecs", "cleanPerf"]);
